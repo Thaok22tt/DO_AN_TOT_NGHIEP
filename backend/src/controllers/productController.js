9119bb1f8eff8e0cf592a876ae7a98f01baee564
Thaok22tt <cynex764@gmail.com>
2026-06-05 19:29:30 +0700

    fix: dùng CLOUDINARY_URL để config

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -5,6 +5,7 @@ const categoryModel = require('../models/categoryModel')
 const productModel = require('../models/productModel')
 
 cloudinary.config({
+  cloudinary_url: process.env.CLOUDINARY_URL,
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
